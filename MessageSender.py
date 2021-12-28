cca4ea5ca74c37d8eb8ef17cbba62b354e0cefa4
vallenov <a2lenov@mail.ru>
2021-12-28 23:31:45 +0300

    Теперь проект запускается как сервер и принимает POST запросы

--- MessageSender.py
+++ MessageSender.py
@@ -1,43 +1,49 @@
 import logging
 import configparser
-
+from flask import Flask
+from flask_restful import request
 import smtplib as smtp
 
-class MessageSender:
-    def __init__(self):
-        self.logging = logging
-        self.logging.basicConfig(filename='run.log',
-                            level=logging.INFO,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
-        self.config = configparser.ConfigParser()
-        self.config.read('MessageSender.ini', encoding="windows-1251")
-        self.sender = self.config['MAIL']['login']
-        self.passwd = self.config['MAIL']['passwd']
-        self.server = smtp.SMTP(self.config['MAIL']['SMTP'], self.config['MAIL']['port'])
-        self.server.starttls()
+app = Flask(__name__)
 
-    def send(self, mail: dict) -> None:
-        '''
-        Input:
-        dict like {'to': ...,
-                   'subject': ...,
-                   'text': ...
-        '''
-        to = mail['to']
-        subj = mail['subject']
-        text = mail['text']
-        try:
-            self.server.login(self.sender, self.passwd)
-            self.server.sendmail(self.sender, to, f'Subject:{subj}\n{text}')
-        except Exception as ex:
-            self.logging.exception(f'Exception: {ex}')
-        else:
-            self.logging.info('message has been sent')
+
+@app.route('/', methods=['POST'])
+def send() -> dict:
+    '''
+    Input:
+    dict like {'to': ...,
+               'subject': ...,
+               'text': ...
+    '''
+    resp = {}
+    logging.basicConfig(filename='run.log',
+                             level=logging.INFO,
+                             format='%(asctime)s - %(levelname)s - %(message)s')
+    config = configparser.ConfigParser()
+    config.read('MessageSender.ini', encoding="windows-1251")
+    sender = config['MAIL']['login']
+    passwd = config['MAIL']['passwd']
+    server = smtp.SMTP(config['MAIL']['SMTP'], int(config['MAIL']['port']))
+    server.starttls()
+    to = request.form.get('to')
+    if not to:
+        resp['res'] = 'ERROR'
+        resp['descr'] = 'Email address is not found'
+        return resp
+    subj = request.form.get('subject')
+    text = request.form.get('text')
+    try:
+        server.login(sender, passwd)
+        server.sendmail(sender, to, f'Subject:{subj}\n{text}')
+    except Exception as ex:
+        logging.exception(f'Exception: {ex}')
+        resp['res'] = 'ERROR'
+        resp['descr'] = 'Mail do not sent'
+        return resp
+    else:
+        logging.info('message has been sent')
+        resp['resp'] = 'OK'
+        return resp
 
 if __name__ == "__main__":
-    MS = MessageSender()
-    m = {}
-    m['to'] = 'something@mail.ru'
-    m['subject'] = 'Info'
-    m['text'] = 'Test'
-    MS.send(m)
+    app.run(debug=True)
