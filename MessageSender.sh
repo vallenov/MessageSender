78bf84eb0e55c2c3af401d61976e0b6e51844a92
vallenov <a2lenov@mail.ru>
2022-06-16 01:05:59 +0300

    Исправлен скрипт запуска

--- MessageSender.sh
+++ MessageSender.sh
@@ -4,9 +4,9 @@ WORK_DIR=/home/vladimir/prog/python/projects/MessageSender
 
 cd $WORK_DIR
 #. .venv/bin/activate && echo activate venv
-pyenv activate MessageSender
+pyenv activate MessageSender && echo activate venv
 export FLASK_APP=MessageSender.py && echo export vars
+echo Message server is start
 #./.venv/bin/flask run --host=0.0.0.0 --port=5111
 ~/.pyenv/versions/MessageSender/bin/flask run --host=0.0.0.0 --port=5111
 #python MessageSender.py
-echo Message server is start
